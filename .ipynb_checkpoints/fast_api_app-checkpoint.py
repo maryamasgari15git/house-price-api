efa64fa86b71948bce6216907f2b61868a345115
maryam <maryam.asgari15@gmail.com>
2025-11-28 16:26:10 +0330

    change requirment

--- .ipynb_checkpoints/fast_api_app-checkpoint.py
+++ .ipynb_checkpoints/fast_api_app-checkpoint.py
@@ -14,7 +14,7 @@ from datetime import datetime
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.requests import Request
-from openai 
+import openai 
 import os
 #from deepseek import DeepSeekAPI
 
